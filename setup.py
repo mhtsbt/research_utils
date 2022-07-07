ec7290db4d010851ac1b2becee031e2257998a72
mhtsbt <matthias.hutsebaut@gmail.com>
2022-07-07 10:15:08 +0200

    module structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="research_utils",
+    version="0.0.1",
+    description="A collection of common utils, used during my research",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6'
+)
